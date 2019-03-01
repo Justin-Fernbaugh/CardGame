44a079aea37f846b507dc17d2f13d01c6a440295
Justin-Fernbaugh <fernbaughj@gmail.com>
2019-03-01 14:23:19 -0800

    Add pretext for stopwatch card.

--- Main.java
+++ Main.java
@@ -11,20 +11,18 @@ class Main
     public static Player player2 = new Player(20, 2);
 
     //name, damage, manaCost, (Optional) effect
-    public static Card devCard = new Card("devCard", 10, 2);
+    public static Card devCard = new Card("devCard", player1.getHealth(), 2);
     public static Card fireball = new Card("FireBall", 5, 2);
     public static Card ice = new Card("Ice", 5, 2);
+    public static Card stopwatch = new Card("Stopwatch", 0, 10, 1);
 
     //
     public static ArrayList<Card> allCards = new ArrayList<Card>();
     //
     public static void main(String[] args) 
     {
-        allCards.add(devCard);
-        player1.addCardToInventory(devCard);
-        player2.addCardToInventory(devCard);
-        player1.addCardToInventory(devCard);
-        player2.addCardToInventory(devCard);
+        player2.addCardToInventory(fireball);
+        player1.addCardToInventory(fireball);
 
         //
         startGame();
@@ -33,6 +31,13 @@ class Main
 
     public static void startupEntries()
     {
+        //Adds all cards to a ArrayList
+        allCards.add(fireball);
+        allCards.add(ice);
+        allCards.add(stopwatch);
+        
+
+
         System.out.println(ANSI_RED + "Card Game v0.01" + ANSI_RESET);
         System.out.println("The game plays in turns with each player getting an action.");
 
@@ -72,20 +77,32 @@ class Main
                 cardsToString += cardsToStringArray[i];
             }
         }
+
+
         System.out.println(String.format("What card would %s player" + playerToDecide.getPlayerInt() + " %slike to play? (%s)", ANSI_RED, ANSI_RESET, cardsToString));
         String userInput = scanner.nextLine().toLowerCase();
         //Check if user input is equal to card option in cardsToStringArray. - DONE
-        for(int i = 0; i < playerToDecide.getInventory().size(); i++)
+        while(true)
         {
-            if(userInput.equals(cardsToStringArray[i]))
+            for(int i = 0; i < playerToDecide.getInventory().size(); i++)
             {
-                int damageToGive = playerToDecide.getInventory().get(i).getDamage();
-                opponent.takeDamage(damageToGive);
-                playerToDecide.removeCardFromInventory(i);
-                System.out.println(String.format("Player%s has taken %s damage, and now has %s health.", opponent.getPlayerInt(), damageToGive, opponent.getHealth()));
-                break;
+                if(userInput.equals(cardsToStringArray[i]))
+                {
+                    int damageToGive = playerToDecide.getInventory().get(i).getDamage();
+                    opponent.takeDamage(damageToGive);
+                    playerToDecide.removeCardFromInventory(i);
+                    System.out.println(String.format("Player%s has taken %s damage, and now has %s health.", opponent.getPlayerInt(), damageToGive, opponent.getHealth()));
+                    break;
+                }
+                else if(userInput.equals("{devCard}"))
+                {
+                    opponent.takeDamage(devCard.getDamage());
+                    System.out.println(String.format("Player%s has taken %s damage, and now has %s health.", opponent.getPlayerInt(), devCard.getDamage(), opponent.getHealth()));
+                    break;
+                }
             }
         }
+        
 
     }
 
